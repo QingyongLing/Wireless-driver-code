4b1174e3dbaf5eb952819f2fd0cff616cf2400d0
Ling <2682451149@qq.com>
2018-02-28 21:16:17 +0800

    swba

--- ath9k/tdma.c
+++ ath9k/tdma.c
@@ -1,13 +1,21 @@
 //修改 2018.2.19
+//2018.2.28 add tdma_set_SWBA
 #include <net/tdma.h>
 #include "ath9k.h"
+void tdma_set_SWBA(struct ieee80211_local *local,int period){
+    struct ieee80211_hw *hw=&(local->hw);
+    struct ath_softc *sc = hw->priv;
+    struct ath_hw *ah=sc->sc_ah;
+    REG_WRITE(ah, AR_SWBA_PERIOD, period);
+}
+
 void tdma_tasklet(unsigned long data)
 {
     static int slot=0;
     struct ath_softc *sc = (struct ath_softc *)data;
     struct ieee80211_hw *hw=sc->hw;
     struct ath_hw *ah=sc->sc_ah;
-    struct ieee80211_local *local = hw_to_local(hw);
+    //struct ieee80211_local *local = hw_to_local(hw);
     if(ah->opmode==NL80211_IFTYPE_AP){
         ++slot;
         if(slot==1){
--- include/net/tdma.h
+++ include/net/tdma.h
@@ -1,2 +1,5 @@
 //修改 2018.2.19
+//2018.2.28 add tdma_set_SWBA
+struct ieee80211_local;
+void tdma_set_SWBA(struct ieee80211_local *local,int period);
 void tdma_tasklet(unsigned long data);
