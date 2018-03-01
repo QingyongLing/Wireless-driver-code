036a13c4f3da9bea94b91e494cefc06ae09872ed
Ling <2682451149@qq.com>
2018-03-01 16:44:36 +0800

    SWBA

--- ath9k/main.c
+++ ath9k/main.c
@@ -2638,12 +2638,14 @@ void tdma_set_SWBA(struct ieee80211_hw *hw,int period){
     struct ath_softc *sc = hw->priv;
     struct ath_hw *ah=sc->sc_ah;
 	int flags=0;
+	u64 tsf=0;
 	flags |=AR_SWBA_TIMER_EN;
 	ah->imask |= ATH9K_INT_SWBA;
     REG_WRITE(ah, AR_SWBA_PERIOD, period);
 	REGWRITE_BUFFER_FLUSH(ah);
 	REG_SET_BIT(ah, AR_TIMER_MODE, flags);
-	printk("Set SWBA\n");
+	tsf = ath9k_hw_gettsf64(ah);
+	printk("Set SWBA at tsf %llu\n",tsf);
 }
 
 struct ieee80211_ops ath9k_ops = {
--- ath9k/tdma.c
+++ ath9k/tdma.c
@@ -9,7 +9,6 @@ void tdma_tasklet(unsigned long data)
     struct ieee80211_hw *hw=sc->hw;
     struct ath_hw *ah=sc->sc_ah;
     //struct ieee80211_local *local = hw_to_local(hw);
-    printk("tdma_tasklet scheduled\n");
     if(ah->opmode==NL80211_IFTYPE_AP){
         ++slot;
         if(slot==1){
@@ -23,7 +22,9 @@ void tdma_tasklet(unsigned long data)
     }else if(ah->opmode==NL80211_IFTYPE_STATION){
         static int count=0;
         ++count;
-        printk("SWBA activate--------------------%d\n",count);
+        u64 tsf=0;
+        tsf = ath9k_hw_gettsf64(ah);
+        printk("SWBA activate %d times at %llu\n",count,tsf);
         if(count==1000)count=0;
         ah->imask &= ~ATH9K_INT_SWBA;
     }
