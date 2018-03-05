7d03fcd90b4f6d836128b344a47224795b4f7144
Ling <2682451149@qq.com>
2018-03-05 21:36:00 +0800

    tsf

--- ath9k/tdma.c
+++ ath9k/tdma.c
@@ -11,13 +11,21 @@ void tdma_tasklet(unsigned long data)
     //struct ieee80211_local *local = hw_to_local(hw);
     if(ah->opmode==NL80211_IFTYPE_AP){
         ++slot;
+        u64 tsf = ath9k_hw_gettsf64(ah);
+        static bool flag=true;
         if(slot==1){
+            if(flag){
+                flag=false;
+                printk("flag is true,slot is %d and tsf is %llu",slot,tsf);
+            }
             ath9k_beacon_tasklet(data);
         }else{
-            //printk("++++++++++++++++++++++++++\n");
+            if(!flag){
+                flag=true;
+                printk("flag is true, slot is %d and tsf is %llu",slot,tsf);
+            }
             tdma_send_data(hw);
             if(slot==50)slot=0;
-            //printk("-----------------------------\n");
         }
     }else if(ah->opmode==NL80211_IFTYPE_STATION){
         static int count=0;
