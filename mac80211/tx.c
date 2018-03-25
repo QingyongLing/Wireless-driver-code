156399ad8cbcb7c5191788992e39fd73bc5a2ca4
Ling <2682451149@qq.com>
2018-03-25 16:12:19 +0800

    sys

--- mac80211/tx.c
+++ mac80211/tx.c
@@ -1486,7 +1486,7 @@ static bool ieee80211_tx_frags(struct ieee80211_local *local,
     if(vif->type==NL80211_IFTYPE_AP)
 	    return ieee80211_tx_frags_byAP(local,vif,sta,skbs,txpending);
 
-	if(vif->type==NL80211_IFTYPE_STATION){
+	if(vif->type==NL80211_IFTYPE_STATION&&vif->bss_conf.assoc){
         return ieee80211_tx_frags_bySTA(local,vif,sta,skbs,txpending);
 	}
 
